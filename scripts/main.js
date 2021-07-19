3acdfe8407926d73c2c14600945e0e69dbcb29b0
okaycodes <fo.okenana@gmail.com>
2021-07-19 07:26:40 +0100

    update main.js: create getCoin function

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,23 @@
 const bodyEl = document.getElementById('body')
 
 
-function getBackground(){
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-  .then(res => res.json())
-  .then(data => {
-    const url = data.urls.raw
-    let img = new Image()
-    img.onload = function () {
+async function getBackground(){
+  const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+  const data = await response.json()
+  const url = data.urls.raw
+  let img = new Image()
+  img.onload = function () {
       bodyEl.style.backgroundImage = `url(${url})`
-    }
-    img.src =  url
-})
+  }
+  img.src =  url
 }
 // let interval = setInterval (getBackground, 8000)
 setTimeout(getBackground, 8000)
+
+
+async function getCoin(){
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  const data = await response.json()
+  const imgUrl = data.image.thumb
+  const  currentPrice = data.market_data.current_price.usd
+}
