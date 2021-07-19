bd40a6fdce6fc567a5c75d6acbd087c1692aa209
okaycodes <fo.okenana@gmail.com>
2021-07-19 06:39:01 +0100

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,17 @@
 const bodyEl = document.getElementById('body')
 
 
-
+function getBackground(){
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(res => res.json())
   .then(data => {
-          const url = `url(${data.urls.raw})`
-  // let image = new Image()
-  // image.src = url
-  bodyEl.style.backgroundImage = url
+    const url = data.urls.raw
+    let img = new Image()
+    img.onload = function () {
+      bodyEl.style.backgroundImage = `url(${url})`
+    }
+    img.src =  url
 })
-
-// let interval = setInterval (getBackground, 4000)
-setTimeout(getBackground(), 3000)
+}
+// let interval = setInterval (getBackground, 8000)
+setTimeout(getBackground, 8000)
