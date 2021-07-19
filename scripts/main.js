91c52bd4f6a6ac5b02d894420d54ea10717a66f6
okaycodes <fo.okenana@gmail.com>
2021-07-19 15:27:54 +0100

    update index.html, main.js, main.css

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,35 @@ const dogecoinEl = document.getElementById('dogecoin-data')
 const bgCreatorEl = document.getElementById('bg-creator')
 const bgLocationEl = document.getElementById('bg-location')
 
-async function getBackground(){
-  const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-  const data = await response.json()
-  const url = data.urls.full
-  const img = new Image()
-  img.src =  url
-  img.onload = function () {
-      bodyEl.style.backgroundImage = `url(${url})`
-      bgCreatorEl.textContent = `By: ${data.user.name}`
-      bgLocationEl.textContent = `Location ${data.location.name}`
-      getCoin()
-  }
+function getBackground(){
+  fetch("https://apis.scrimba.ocom/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(response => response.json())
+    .then(data => {
+      const url = data.urls.full
+      const img = new Image()
+      img.src =  url
+      img.onload = function () {
+          bodyEl.style.backgroundImage = `url(${url})`
+          bgCreatorEl.textContent = `By: ${data.user.name}`
+          bgLocationEl.textContent = `Location: ${data.location.name}`
+          getCoin()
+      }
+    }).catch( err => {
+      console.log(err)
+      const url = "https://images.unsplash.com/photo-1486673748761-a8d18475c757?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjY2ODUzODQ&ixlib=rb-1.2.1&q=85"
+      const img = new Image()
+      img.src =  url
+      img.onload = function () {
+          bodyEl.style.backgroundImage = `url(${url})`
+          bgCreatorEl.textContent = `By: Karsten Würth`
+          bgLocationEl.textContent = `Location: Alsheim, Germany`
+          getCoin()
+      }
+    })
 }
 // let interval = setInterval (getBackground, 8000)
-setTimeout(getBackground, 800)
+// setTimeout(getBackground, 800)
+getBackground()
 
 
 async function getCoin(){
@@ -44,5 +58,17 @@ async function getCoin(){
 
 function currentTime(){
   date = new Date()
-  timeEl.textContent = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  let time = document.createElement('p')
+  let input = document.createElement('input')
+  let line = document.createElement('hr')
+  time.textContent = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  input.placeholder = "Enter Weather Location e.g. Lagos, New York"
+  timeEl.appendChild(time)
+  timeEl.appendChild(input)
+  // timeEl.appendChild(line)
 }
+
+
+fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather?q=Lagos&units=imperial")
+    .then(response => response.json())
+    .then(data=>{console.log(data)})
