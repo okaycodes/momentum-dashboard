8b4e057a61bbb97fb7103d094c8d9276e82a702f
okaycodes <fo.okenana@gmail.com>
2021-07-24 19:54:31 +0100

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,12 @@
 
-const bodyEl = document.getElementById('body')
+// const bodyEl = document.getElementById('body')
 const timeEl = document.getElementById('time-data')
 const inputEl = document.getElementById('input')
-const coinIdEl = document.getElementById('coin-id')
 const weatherEl = document.getElementById('weather')
 const coinImgEl = document.getElementById('coin-img')
-const dogecoinEl = document.getElementById('dogecoin-data')
 const settingsEl = document.getElementById('settings')
-const bgCreatorEl = document.getElementById('bg-creator')
-const bgLocationEl = document.getElementById('bg-location')
+// const bgCreatorEl = document.getElementById('bg-creator')
+// const bgLocationEl = document.getElementById('bg-location')
 const weatherIconEl = document.getElementById('weather-icon')
 const weatherLocationEl = document.getElementById('weather-location')
 const weatherDescriptionEl = document.getElementById('weather-desc')
@@ -30,6 +28,7 @@ function getBackground(){
           bgCreatorEl.textContent = `Pic by: ${data.user.name}`
           bgLocationEl.textContent = `Location: ${data.location.name}`
           getCoin()
+          getQuote()
       }
     }).catch( err => {
       console.log(err)
@@ -41,6 +40,7 @@ function getBackground(){
           bgCreatorEl.textContent = `Pic by: Karsten Würth`
           bgLocationEl.textContent = `Location: Alsheim, Germany`
           getCoin()
+          getQuote()
       }
     })
 }
@@ -51,34 +51,45 @@ getBackground()
 
 async function getCoin(){
   const coinArr = ['bitcoin', 'dogecoin', 'ethereum', 'litecoin', 'cardano']
-  let randomCoin = coinArr[Math.floor(Math.random()*coinArr.length)]
+  const randomCoin = coinArr[Math.floor(Math.random()*coinArr.length)]
   const response = await fetch(`https://api.coingecko.com/api/v3/coins/${randomCoin}`)
-  console.log(randomCoin)
   const data = await response.json()
   const imgUrl = data.image.thumb
   coinImgEl.src = imgUrl
   coinImgEl.onload = function(){
-    const coinDataEl = document.createElement('ul')
     const html = `<li> 🎯:  $${data.market_data.current_price.usd}</li>
-            <li> ↗️:  $${data.market_data.high_24h.usd}</li>
-            <li> ↘️:  $${data.market_data.low_24h.usd}</li>`
-    coinDataEl.innerHTML = html
-    coinIdEl.textContent = `${data.id}`
-    dogecoinEl.appendChild(coinDataEl)
+                  <li> ↗️:  $${data.market_data.high_24h.usd}</li>
+                  <li> ↘️:  $${data.market_data.low_24h.usd}</li>`
+    document.getElementById('coin-id').textContent = data.id
+    document.getElementById('coin-data').innerHTML = html
     let timeInterval = setInterval(currentTime, 1000)
   }
 }
 
 
 function currentTime(){
-  date = new Date()
-  let time = document.createElement('p')
-  let options1 = { hour: 'numeric', minute: 'numeric', hour12: true }
-  let options2 = { timeStyle: 'medium'}
-  time.textContent = date.toLocaleString('en-US', options1 )
-  timeEl.removeChild(timeEl.firstChild)
-  timeEl.appendChild(time)
+  const date = new Date()
+  const options1 = { hour: 'numeric', minute: 'numeric', hour12: true }
+  const options2 = { timeStyle: 'medium'}
+  timeEl.innerHTML = ""
+  timeEl.textContent = date.toLocaleString('en-US', options1 )
 }
+
+function getQuote(){
+  fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(data => {
+        let blockquoteEl = document.createElement('blockquote')
+        let quoteCaptionEl = document.createElement('figcaption')
+        let seeMoreEl = document.createElement('span')
+        blockquoteEl.cite = "https://api.quotable.io/random"
+        blockquoteEl.textContent = `${data.content}`
+        quoteCaptionEl.textContent = `-${data.author}`
+        quoteEl.appendChild(blockquoteEl)
+        quoteEl.appendChild(quoteCaptionEl)
+      })
+}
+
 
 function getWeather(){
   fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?q=${weatherLocation}&units=metric`)
@@ -131,20 +142,3 @@ function hideBar(){
     }
   })
 }
-
-
-
-// quotes
-
-fetch("https://api.quotable.io/random")
-    .then(response => response.json())
-    .then(data => {
-      let blockquoteEl = document.createElement('blockquote')
-      let quoteCaptionEl = document.createElement('figcaption')
-      let seeMoreEl = document.createElement('span')
-      blockquoteEl.cite = "https://api.quotable.io/random"
-      blockquoteEl.textContent = `${data.content}`
-      quoteCaptionEl.textContent = `-${data.author}`
-      quoteEl.appendChild(blockquoteEl)
-      quoteEl.appendChild(quoteCaptionEl)
-    })
